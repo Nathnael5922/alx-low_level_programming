f74d80c6d8b8125242e1c52f6a86ee7a2bc6b880
babi <babiwubi@gmail.com>
2022-12-08 13:39:23 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<studio.h>
 /**
- * main - Entry point
- * Return: Always 0     
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+puts("\"Programing is like building a multi lingual puzzle");
+return (0);
 }
