3516e00be5d4155d7f6c470805a8001be2a2436d
babi <babiwubi@gmail.com>
2022-12-08 15:42:50 -0800

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
-* main - Entry point
+* main - prints exactly and that piece of art is useful 
 * Return: Always 1
 */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful
-	\" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
