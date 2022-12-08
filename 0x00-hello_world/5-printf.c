ff6999618ff9536470c3815b53597d500c0110e5
babi <babiwubi@gmail.com>
2022-12-08 14:47:36 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - Entry
- * Return: always 0
- */
+* main - Entry point
+* Return: Always 0
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
