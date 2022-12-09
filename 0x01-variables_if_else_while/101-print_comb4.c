c4d0f6f71d9935999fd888fe4a72236d2cc9110a
babi <babiwubi@gmail.com>
2022-12-09 14:52:19 -0800

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,7 +7,7 @@
 
 /**
 * main - Prints all possible combinations of three different digits,
-* in ascending order, separated by a comma followed by a space.
+*        in ascending order, separated by a comma followed by a space.
 * Return: Always 0.
 */
 int main(void)
@@ -17,7 +17,7 @@ int main(void)
 	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{	
+		{
 			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
 				putchar((digit1 % 10) + '0');
@@ -25,15 +25,15 @@ int main(void)
 				putchar((digit3 % 10) + '0');
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-			
+					continue;	
+				
 				putchar(',');
 				putchar(' ');
-			}	
+			}
 		}
 	}
 
-	putchar('\n');
+	0putchar('\n');
 
 	return (0);
-}	
+}
