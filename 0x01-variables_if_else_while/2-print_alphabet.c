da523d9686fb255d1b971654b24b80a5c0f3431b
babi <babiwubi@gmail.com>
2022-12-09 11:45:05 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/**
+* is a program that prints latters
+* in lowercase
+* */
+
+#include <stdio.h>
+/**
+ *  * main - Prints the alphabet in lowercase.
+ *   *
+ *    * Return: Always 0.
+ *     */
+
+int main(void)
+
+	char latter;
+
+	for (latter = 'a'; latter <= 'z' latter ++);
+		putchar(latter);
+
+	putchar("\n");
+	
+	return (0);
+}	
