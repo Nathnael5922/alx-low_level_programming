6b6d6b5c0efe7cbe6b5ffce8e65bc38ffe02b7fa
babi <babiwubi@gmail.com>
2022-12-16 11:49:06 -0800

    0

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,10 @@
  */
 void print_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i <= 9; i++)
-		_putchar(i);
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
 
-	_putchar("\n");
+	_putchar('\n');
 }
