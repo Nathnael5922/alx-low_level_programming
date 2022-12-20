41c56121630a1c23808ee3852aed641e5944b62e
babi <babiwubi@gmail.com>
2022-12-20 14:20:22 -0800

    0

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,5 +7,16 @@
       */
 int _atoi(char *s)
 {
-	return (0);
+	int i = 1;
+
+	unsigned int num = 0;
+	do {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num * i);
 }
