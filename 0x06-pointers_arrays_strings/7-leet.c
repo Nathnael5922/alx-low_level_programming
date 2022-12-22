4df18c0a451cb92970c91d71b639ab5f33ab224c
babi <babiwubi@gmail.com>
2022-12-22 13:04:42 -0800

    0

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,16 @@
   */
 char *leet(char *str)
 {
-	int index1 = 0, index2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
-	while (str[++index1])
+	for (i = 0; *(s + i); i++)
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (str[index1] == leet[index2] ||
-			str[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
+			if (a[j] == s[i])
+				[i] = b[j];
 		}
 	}
 	return (str);
